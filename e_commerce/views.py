f0eaa0d32df1c1139a4b408ae4ca115d4e0c97e1
Ivanildo <geficogestor@gmail.com>
2019-04-24 15:45:09 -0300

    construcao das views contato e sobre

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,4 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def home_page(request):
-   return render(request,"home_page.html",{})
+    context = {
+        "title": "Página Principal",
+        "content": "Bem vindo a página principal"
+    }
+    return render(request,"home_page.html",context)
+
+def about_page(request):
+    context = {
+        "title": "Página sobre",
+        "content": "Bem vindo a página sobre"
+    }
+    return render(request,"about/view.html",context)
+
+def contact_page(request):
+     context = {
+        "title": "Página Contato",
+        "content": "Bem vindo a página contato"
+     }
+     return render(request,"contact/view.html",context) 
